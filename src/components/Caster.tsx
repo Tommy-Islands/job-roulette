3290bac71679b68d1072b3b30e6b7f5e741073dc
Tommy <tomo.zu_shima@icloud.com>
2025-02-24 16:11:08 +0900

    完成(公開用)

--- src/components/Caster.tsx
+++ src/components/Caster.tsx
@@ -6,7 +6,7 @@ const CASTER_JOBS = [
   { label: '黒魔道士', icon: '/job/magicalrangedDPS/BlackMage.png' },
   { label: '召喚士', icon: '/job/magicalrangedDPS/Summoner.png' },
   { label: '赤魔道士', icon: '/job/magicalrangedDPS/RedMage.png' },
-  { label: 'ピクトマンサー', icon: '/job/magicalrangedDPS/Pictomancer.png' },
+  // { label: 'ピクトマンサー', icon: '/job/magicalrangedDPS/Pictomancer.png' },
 ] as const;
 
 export const CasterRoleSelector = forwardRef<RoleRef, RoleSelectorProps>(
